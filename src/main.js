f6e14224de08b5a48736c00bb38d587a636bb78f
Icomanman <fred_barabs@yahoo.com>
2023-02-25 22:24:54 +0800

    feat: basic 3-layer model ran; poor results.

--- src/main.js
+++ src/main.js
@@ -11,11 +11,14 @@ const data_path = path.resolve(process.cwd(), 'src/dat');
 const src_dat = `${data_path}/64923022023.txt`;
 const dat = fs.readFileSync(`${src_dat}`, 'utf-8');
 
-const LEARNING_RATE = 0.0001;
-const OPTIMISER = tf.train.adam(LEARNING_RATE);
+const LEARNING_RATE = 0.000001;
+const OPTIMISER = tf.train.sgd(LEARNING_RATE);
 
 const logProgress = (epoch, logs) => {
     console.log('Data for epoch ' + epoch, Math.sqrt(logs.loss));
+    if (epoch == 15) {
+        OPTIMISER.setLearningRate(LEARNING_RATE / 2);
+    }
 }
 
 const readDataArr = async (file) => {
@@ -57,18 +60,19 @@ const readDataArr = async (file) => {
     const data_arr = await readDataArr(src_dat);
     const INPUT_DATA = [];
     for (let i = 0; i < data_arr.length; i++) {
-        // const new_arr = JSON.parse(JSON.stringify(base_arr));
-        // tf.util.shuffle(new_arr)
-        // INPUT_DATA.push(new_arr);
-        INPUT_DATA.push(base_arr);
+        const new_arr = JSON.parse(JSON.stringify(base_arr));
+        tf.util.shuffle(new_arr)
+        INPUT_DATA.push(new_arr);
+        // INPUT_DATA.push(base_arr);
     }
 
     const INPUT_TENSOR = tf.tensor2d(INPUT_DATA);
-    const OUTPUT_TENSOR = tf.tensor1d(data_arr);
+    const OUTPUT_TENSOR = tf.tensor2d(data_arr);
 
     const model = tf.sequential({
         layers: [
-            tf.layers.dense({ inputShape: [BASE], units: BASE, activation: 'relu' }),
+            tf.layers.dense({ inputShape: [BASE], units: 28, activation: 'relu' }),
+            tf.layers.dense({ units: 28, activation: 'relu' }),
             tf.layers.dense({ units: 6 }),
         ]
     });
@@ -82,12 +86,12 @@ const readDataArr = async (file) => {
     let results = await model.fit(INPUT_TENSOR, OUTPUT_TENSOR, {
         callbacks: { onEpochEnd: logProgress },
         shuffle: true,
-        batchSize: 20,
-        epochs: 100,
+        batchSize: 10,
+        epochs: 30,
     });
 
     tf.tidy(function () {
-        let newInput = INPUT_TENSOR;
+        let newInput = tf.tensor2d([INPUT_DATA[0]]);
         let output = model.predict(newInput);
         output.print();
     });
